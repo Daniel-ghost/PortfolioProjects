93e5e23ef222b93ec9f66b966916fe715059b066
Daniel Abraham Jangbot <abramdan555@gmail.com>
2024-11-21 06:07:51 +0100

    Add files via upload

--- project V2.sql
+++ project V2.sql
@@ -1,115 +1,89 @@
-SELECT TOP (1000) [iso_code]
-      ,[continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[total_cases]
-      ,[new_cases]
-      ,[new_cases_smoothed]
-      ,[total_deaths]
-      ,[new_deaths]
-      ,[new_deaths_smoothed]
-      ,[total_cases_per_million]
-      ,[new_cases_per_million]
-      ,[new_cases_smoothed_per_million]
-      ,[total_deaths_per_million]
-      ,[new_deaths_per_million]
-      ,[new_deaths_smoothed_per_million]
-      ,[reproduction_rate]
-      ,[icu_patients]
-      ,[icu_patients_per_million]
-      ,[hosp_patients]
-      ,[hosp_patients_per_million]
-      ,[weekly_icu_admissions]
-      ,[weekly_icu_admissions_per_million]
-      ,[weekly_hosp_admissions]
-      ,[weekly_hosp_admissions_per_million]
-  FROM [PortfolioProject].[dbo].['COVID Deaths$']
 
-  select *
-  from ['COVID Deaths$']
-  where continent is not null
 
-  select location, date, total_cases, new_cases, total_deaths, population
-  from dbo.['COVID Deaths$']
-  order by 1, 3 asc
+  SELECT *
+  FROM [COVID Deaths]
+  WHERE continent is not null
+
+  SELECT location, date, total_cases, new_cases, total_deaths, population
+  FROM dbo.[COVID Deaths]
+  ORDER BY 1, 3 
 
   --TOTAL CASES VS TOTAL DEATHS. LIKELYHOOD OF DEATH FROM COVID IN COUNTRIES
 
-   select location, date, total_cases, total_deaths, population, (total_deaths/total_cases)*100
-  from dbo.['COVID Deaths$']
-  where total_cases > 0 and total_deaths > 0 and location = 'united states'
-  order by 1, 2
+  SELECT location, date, total_cases, total_deaths, population, (total_deaths/total_cases)*100
+  FROM dbo.[COVID Deaths]
+  WHERE total_cases > 0 and total_deaths > 0 and location = 'united states'
+  ORDER BY 1, 2
 
 
   --TOTAL CASES VS POPULATION
 
-  select location, date, total_cases, population, (total_cases/population)*100
-  from dbo.['COVID Deaths$']
-  where total_cases > 0 and location = 'united states'
-  order by 1, 3 asc
+  SELECT location, date, total_cases, population, (total_cases/population)*100
+  FROM dbo.[COVID Deaths]
+  WHERE total_cases > 0 and location = 'united states'
+  ORDER BY 1, 3 asc
 
-  select location, total_cases
-  from ['COVID Deaths$']
-  order by total_cases desc
+  SELECT location, total_cases
+  FROM [COVID Deaths]
+  ORDER BY total_cases desc
 
 
   --COUNTRIES WITH HIGHEST INFECTION RATES TO POPULATION
   
-  select location, population, max(total_cases) highestinfcount, max((total_cases/population))*100 percentageofpopulationinf
-  from dbo.['COVID Deaths$']
-  group by location, population
-  order by percentageofpopulationinf desc
+  SELECT location, population, max(total_cases) highestinfcount, max((total_cases/population))*100 percentageofpopulationinf
+  FROM dbo.[COVID Deaths]
+  GROUP BY location, population
+  ORDER BY percentageofpopulationinf desc
 
   --COUNTRIES WITH HIGHEST DEATH COUNT TO POPULATION
 
-  select location, population, max(total_deaths) TotalDeathCount
-  from dbo.['COVID Deaths$']
-  where continent is not null
-  group by location, population
-  order by TotalDeathCount desc
+  SELECT location, population, max(total_deaths) TotalDeathCount
+  FROM dbo.[COVID Deaths]
+  WHERE continent is not null
+  GROUP BY location, population
+  ORDER BY TotalDeathCount desc
 
   --CONTINENTS WITH DEATH COUNT
 
-  select location, max( cast (total_deaths as int)) TotalDeathCount
-  from dbo.['COVID Deaths$']
-  where continent is null
-  group by location
-  order by TotalDeathCount desc
+  SELECT location, max( cast (total_deaths as int)) TotalDeathCount
+  FROM dbo.[COVID Deaths]
+  WHERE continent is null
+  GROUP BY location
+  ORDER BY TotalDeathCount desc
 
 
   --TOTAL DEATHS IN AFRICAN COUNTRIES
 
-  select location, max(total_deaths) TDC
-  from dbo.['COVID Deaths$']
-  where continent = 'africa'
-  group by location
-  order by TDC desc
+  SELECT location, max(total_deaths) TDC
+  FROM dbo.[COVID Deaths]
+  WHERE continent = 'africa'
+  GROUP BY location
+  ORDER BY TDC desc
 
 
-  select continent, avg(total_cases)
-  from dbo.['COVID Deaths$']
-  group by continent
+  SELECT continent, avg(total_cases)
+  FROM dbo.[COVID Deaths]
+  GROUP by continent
 
 
   --GLOBAL NUMBERS
 
   --ERROR, WORK ON
-select date, sum(new_cases) totalcases,sum(new_deaths) totaldeaths, total_deaths/total_cases *100 as DeathPercentage
-from dbo.['COVID Deaths$']
-where new_cases > 0 and new_deaths > 0 and continent is not null
-group by date, total_deaths, total_cases
-order by 1, 2
+SELECT date, sum(new_cases) totalcases,sum(new_deaths) totaldeaths, total_deaths/total_cases *100 as DeathPercentage
+FROM dbo.[COVID Deaths]
+WHERE new_cases > 0 and new_deaths > 0 and continent is not null
+GROUP BY date, total_deaths, total_cases
+ORDER BY 1, 2
 
-select  sum(new_deaths) totalcases, max(total_deaths) totaldeaths--,sum(total_deaths), sum(total_deaths)/sum(total_cases) *100 as DeathPercentage
-from dbo.['COVID Deaths$']
-where new_cases > 0 and new_deaths > 0 and continent is not null
+SELECT  sum(new_deaths) totalcases, max(total_deaths) totaldeaths--,sum(total_deaths), sum(total_deaths)/sum(total_cases) *100 as DeathPercentage
+FROM dbo.[COVID Deaths]
+WHERE new_cases > 0 and new_deaths > 0 and continent is not null
 
-order by 1, 2
+ORDER BY 1, 2
 
-select date, new_cases
-from dbo.['COVID Deaths$']
-where new_cases > 0
+SELECT date, new_cases
+FROM dbo.[COVID Deaths]
+WHERE new_cases > 0
 
 --select date, new_cases, total_cases
 --from dbo.['COVID Deaths$']
@@ -119,10 +93,10 @@ where new_cases > 0
 
 
 
-select location, population, max(total_cases) highestinfxcount, max(total_cases/population)*100 percentpopinfected
-from ['COVID Deaths$']
-group by location, population
-order by percentpopinfected desc
+SELECT location, population, max(total_cases) highestinfxcount, max(total_cases/population)*100 percentpopinfected
+FROM [COVID Deaths]
+GROUP BY location, population
+ORDER BY percentpopinfected desc
 
 
 
@@ -130,24 +104,24 @@ order by percentpopinfected desc
 
 --TEST RUN FOR Partition by
 SELECT vac.new_vaccinations, dea.location, MAX(CONVERT(FLOAT,vac.new_vaccinations)) over (PARTITION by dea.location), MAX(CONVERT(FLOAT,vac.new_vaccinations))
-from ['COVID Vaccination$'] vac join ['COVID Deaths$'] dea
+FROM [portfolioproject].[dbo].[COVID Vaccination] vac join [PortfolioProject].[dbo].[COVID Deaths] dea
 ON dea.location = vac.location
 	and dea.date = vac.date
-where dea.location = 'afghanistan'
-group by dea.location, vac.new_vaccinations
+WHERE dea.location = 'afghanistan'
+GROUP by dea.location, vac.new_vaccinations
 
 
 
 
 --TOTAL POPULATION VS VACCINATION
 
-select dea.continent, dea.location, dea.date,vac.new_vaccinations, dea.population
-from ['COVID Deaths$'] dea join ['COVID Vaccination$'] vac
-on dea.location = vac.location
+SELECT dea.continent, dea.location, dea.date,vac.new_vaccinations, dea.population
+FROM [COVID Deaths] dea join [COVID Vaccination] vac
+ON dea.location = vac.location
 	and dea.date = vac.date
-where dea.continent is not null
+WHERE dea.continent is not null
 and dea.location = 'canada'
-order by  2, 3
+ORDER BY  2, 3
 
 
 --USE CTE
@@ -157,15 +131,15 @@ as
  (
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 SUM(CONVERT(FLOAT,vac.new_vaccinations)) over (PARTITION by dea.location order by dea.location, dea.date) Rollingpeoplevaccinated--, (Rollingpeoplevaccinated/dea.population)*100
-FROM ['COVID Deaths$'] dea join ['COVID Vaccination$'] vac
+FROM [COVID Deaths] dea join [COVID Vaccination] vac
 ON dea.location = vac.location
 	and dea.date = vac.date
 WHERE dea.continent is not null
 --ORDER by  2, 3
 )
-select *, (Rollingpeoplevaccinated/population)*100
+SELECT *, (Rollingpeoplevaccinated/population)*100
 FROM PopvsVac
-order by location, date
+ORDER BY location, date
 
 
 --TEMP TABLE
@@ -185,7 +159,7 @@ Rollingpeoplevaccinated numeric
 INSERT INTO #PercentPopVac
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 SUM(CONVERT(FLOAT,vac.new_vaccinations)) over (PARTITION by dea.location order by dea.location, dea.date) Rollingpeoplevaccinated--, (Rollingpeoplevaccinated/dea.population)*100
-FROM ['COVID Deaths$'] dea join ['COVID Vaccination$'] vac
+FROM [COVID Deaths] dea join [COVID Vaccination] vac
 ON dea.location = vac.location
 	and dea.date = vac.date
 WHERE dea.continent is not null
@@ -202,7 +176,7 @@ FROM #PercentPopVac
 CREATE view PercentPopVac as
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 SUM(CONVERT(FLOAT,vac.new_vaccinations)) over (PARTITION by dea.location order by dea.location, dea.date) Rollingpeoplevaccinated--, (Rollingpeoplevaccinated/dea.population)*100
-FROM ['COVID Deaths$'] dea join ['COVID Vaccination$'] vac
+FROM [COVID Deaths] dea join [COVID Vaccination] vac
 ON dea.location = vac.location
 	and dea.date = vac.date
 WHERE dea.continent is not null
